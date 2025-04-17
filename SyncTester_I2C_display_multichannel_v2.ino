64cdaa335332dad0afbe30b1ce8d06e79d9f21fb
DaCarlo279 <cnestler279@googlemail.com>
2025-04-17 15:48:59 +0200

    Update SyncTester_I2C_display_multichannel_v2.ino
    Erste sauber lauffähige Version

--- SyncTester_I2C_display_multichannel_v2.ino
+++ SyncTester_I2C_display_multichannel_v2.ino
@@ -1,131 +1,128 @@
 #include <Wire.h>
-#include <Adafruit_BMP280.h>
 #include <LiquidCrystal_I2C.h>
+#include <Adafruit_BMP280.h>
 
-// LCD-Display (Adresse ggf. anpassen, z. B. 0x27 oder 0x3F)
-LiquidCrystal_I2C lcd(0x25, 20, 4);
+#define TCAADDR 0x70
+#define NUM_SENSORS 4
+#define HISTORY_SIZE 5
 
-// BMP280 Ã¼ber I2C-Multiplexer (TCA9548A)
+LiquidCrystal_I2C lcd(0x25, 20, 4);  // Deine Display-Adresse
 Adafruit_BMP280 bmp;
-const int NUM_SENSORS = 4;
-const int HISTORY_SIZE = 5;
-const byte TCA_ADDR = 0x70;
 
-// Ringpuffer fÃ¼r geglÃ¤ttete Druckwerte
-float pressureHistory[NUM_SENSORS][HISTORY_SIZE] = {0};
-int historyIndex[NUM_SENSORS] = {0};
+float history[NUM_SENSORS][HISTORY_SIZE];
+uint8_t historyIndex[NUM_SENSORS] = {0};
 
-// Dynamische Kalibrierung (sanft gleitend)
-float calibMin[NUM_SENSORS] = {9999, 9999, 9999, 9999};
-float calibMax[NUM_SENSORS] = {0, 0, 0, 0};
-const float calibrationRate = 0.01; // 1% Anpassung
+const float minPressure = 600.0;
+const float maxPressure = 1050.0;
 
-// Multiplexer-Kanal wÃ¤hlen
-void tcaSelect(uint8_t channel) {
-  if (channel > 7) return;
-  Wire.beginTransmission(TCA_ADDR);
-  Wire.write(1 << channel);
+void tcaselect(uint8_t i) {
+  if (i > 7) return;
+  Wire.beginTransmission(TCAADDR);
+  Wire.write(1 << i);
   Wire.endTransmission();
 }
 
-// BMP280-Druck lesen (in mbar)
-float readPressureFromSensor(uint8_t channel) {
-  tcaSelect(channel);
-  if (!bmp.begin(0x76)) {
-    return -1;
-  }
-  return bmp.readPressure() / 100.0F; // Pa â†’ mbar
-}
-
-// Gleitenden Mittelwert verwalten
 void addToHistory(uint8_t sensor, float value) {
-  pressureHistory[sensor][historyIndex[sensor]] = value;
+  history[sensor][historyIndex[sensor]] = value;
   historyIndex[sensor] = (historyIndex[sensor] + 1) % HISTORY_SIZE;
 }
 
 float getAveragePressure(uint8_t sensor) {
-  float sum = 0;
+  float sum = 0.0;
   for (int i = 0; i < HISTORY_SIZE; i++) {
-    sum += pressureHistory[sensor][i];
+    sum += history[sensor][i];
   }
   return sum / HISTORY_SIZE;
 }
 
-// Balken erzeugen (12 Zeichen + Druckwert)
-String createBar(float pressure, uint8_t sensor) {
-  float pMin = calibMin[sensor];
-  float pMax = calibMax[sensor];
+float readPressureFromSensor(uint8_t channel) {
+  tcaselect(channel);
+  delay(10);
 
-  if (pMax - pMin < 5) {
-    pMin -= 2;
-    pMax += 2;
+  if (!bmp.begin(0x76)) {
+    Serial.print("Sensor ");
+    Serial.print(channel);
+    Serial.println(" Init fehlgeschlagen!");
+    return -1;
   }
 
-  int length = map(pressure, pMin, pMax, 0, 12);
-  length = constrain(length, 0, 12);
+  return bmp.readPressure() / 100.0; // Pa → mbar
+}
 
+String createBar(float value, uint8_t sensorIndex) {
+  int barLength = map(value, minPressure, maxPressure, 0, 16);
+  barLength = constrain(barLength, 0, 16);
   String bar = "";
-  for (int i = 0; i < 12; i++) {
-    bar += (i < length) ? "#" : " ";
+  for (int i = 0; i < barLength; i++) {
+    bar += (char)255;
   }
   return bar;
 }
 
 void setup() {
+  //Serial.begin(115200);
   Wire.begin();
   lcd.init();
   lcd.backlight();
   lcd.clear();
   lcd.print("Druckanzeige Init");
   delay(1000);
+  lcd.clear();
 
-  // Historie initial fÃ¼llen
-  for (uint8_t i = 0; i < NUM_SENSORS; i++) {
-    float p = readPressureFromSensor(i);
-    if (p <= 0) p = 1013.0; // Default-Wert, falls Sensorfehler
+  for (int i = 0; i < NUM_SENSORS; i++) {
     for (int j = 0; j < HISTORY_SIZE; j++) {
-      pressureHistory[i][j] = p;
+      history[i][j] = 1013.0;
     }
-    calibMin[i] = p;
-    calibMax[i] = p;
   }
 }
 
 void loop() {
   for (uint8_t i = 0; i < NUM_SENSORS; i++) {
     float pressure = readPressureFromSensor(i);
+
+    /* Debug: Wo schreiben wir hin?
+    Serial.print(">> LCD-Zeile ");
+    Serial.print(i);
+    Serial.print(" | Sensor ");
+    Serial.println(i);
+    */
+
+    // Zeile löschen
     lcd.setCursor(0, i);
+    lcd.print("                    ");
+    lcd.setCursor(0, i);
+
+    /*
+    Serial.print("Sensor ");
+    Serial.print(i);
+    Serial.print(" | Rohdruck: ");
+    Serial.print(pressure);
+    Serial.print(" mbar");
+    */
 
     if (pressure > 0) {
       addToHistory(i, pressure);
       float avg = getAveragePressure(i);
 
-      // Sanfte Kalibrierung
-      if (avg < calibMin[i]) {
-        calibMin[i] = calibMin[i] * (1.0 - calibrationRate) + avg * calibrationRate;
-      } else {
-        calibMin[i] = calibMin[i] * (1.0 - calibrationRate / 10.0) + avg * (calibrationRate / 10.0);
-      }
+      /*Serial.print(" | Glatt: ");
+      Serial.println(avg);
+      */
 
-      if (avg > calibMax[i]) {
-        calibMax[i] = calibMax[i] * (1.0 - calibrationRate) + avg * calibrationRate;
-      } else {
-        calibMax[i] = calibMax[i] * (1.0 - calibrationRate / 10.0) + avg * (calibrationRate / 10.0);
-      }
-
-      // Balken + Druckwert anzeigen
       String bar = createBar(avg, i);
-      char buf[6];
-      snprintf(buf, sizeof(buf), "%4.0f", avg);
+
+      lcd.setCursor(0, i);
       lcd.print(bar);
-      lcd.print(" ");
-      lcd.print(buf);
+      lcd.setCursor (14, i);
+      lcd.print (" ");
+      lcd.print((int)avg); // nur ganze Zahl, keine Kommas
     } else {
+      Serial.println(" | Fehler beim Auslesen!");
+      lcd.setCursor(0, i);
       lcd.print("Sensor ");
       lcd.print(i);
       lcd.print(": Fehler");
     }
   }
 
-  delay(500);
+  delay(100);
 }
